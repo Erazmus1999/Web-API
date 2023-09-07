1a2cf0065113ae76cb50f84958cc02cfb089b417
Erazmus1999 <erazmus1999@gmail.com>
2023-09-07 12:52:40 +0200

    Adding Character through POST method

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -14,12 +14,25 @@ namespace Web_API.Controllers
         private static List<Character> characters = new List<Character>
         {
             new Character(),
-            new Character{Name = "Brock", Class = Class.Necromancer}
+            new Character{Id = 1, Name = "Brock", Class = Class.Necromancer}
         };
 
-        [HttpGet]
+        [HttpGet("GetAllCharacters")]
         public ActionResult<List<Character>> Get()
         {
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Character> GetSingleCharacter(int id)
+        {
+            return Ok(characters.FirstOrDefault(x => x.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+        {
+            characters.Add(newCharacter);
             return Ok(characters);
         }
     }
